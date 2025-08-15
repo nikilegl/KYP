c86c674ca514520910b276e828506fd8f4442fec
nikilegl <135122115+nikilegl@users.noreply.github.com>
2025-08-15 13:45:23 +0100

    Updated ProjectOverview.tsx

--- src/components/ProjectOverview.tsx
+++ src/components/ProjectOverview.tsx
@@ -346,7 +346,7 @@ export function ProjectOverview({
                           className="h-full rounded-full transition-all duration-300 ease-out"
                           style={{ 
                             width: `${percentage}%`,
-                            backgroundColor: '#A9AFBD'
+                            backgroundColor: '#C5CAD3'
                           }}
                         />
                       </div>
