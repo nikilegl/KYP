4724cfd7d1a8f811a62dfb4b96a8a1e93358ed65
nikilegl <135122115+nikilegl@users.noreply.github.com>
2025-08-30 21:18:19 +0100

    Revert "creating-design-system"

--- src/components/DesignSystem/components/index.ts
+++ src/components/DesignSystem/components/index.ts
@@ -1,4 +1,1 @@
-export { Button, FloatingButton, IconButton, TextButton, GroupButton, SplitButton } from './Button'
-export { ButtonShowcase } from './ButtonShowcase'
-export { TypographyShowcase } from './TypographyShowcase'
-export { ColorShowcase } from './ColorShowcase'
+export { Button } from './Button'
